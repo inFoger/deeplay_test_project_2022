ae71322e8bfbca3a1114bf3863e5c435fa586567
Angald Anton <anton2801z@gmail.com>
2022-11-11 17:13:34 +0600

    [*]Changes in EntityFileReader class

--- DeeplayTestProject2022/src/main/java/com/github/inFoger/entityReading/EntityFileReader.java
+++ DeeplayTestProject2022/src/main/java/com/github/inFoger/entityReading/EntityFileReader.java
@@ -1,6 +1,7 @@
 package com.github.inFoger.entityReading;
 
 import com.github.inFoger.AnimalEntity;
+import com.github.inFoger.Attribute;
 import com.github.inFoger.IAttribute;
 import com.github.inFoger.IEntity;
 
@@ -14,20 +15,36 @@ import java.util.List;
 import java.util.Map;
 
 public class EntityFileReader implements IEntityReader {
+    private IAttribute[] orderedAttributes;
+
+    public EntityFileReader(IAttribute[] orderedAttributes) {
+        this.orderedAttributes = orderedAttributes;
+    }
+
     public List<IEntity> readEntities(String filePath, List<IAttribute> allAttributes) throws IOException {
         List<IEntity> entities = new ArrayList<>();
         try(BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath))){
-            Map<String, String> currentAnimalAttributes = new HashMap<>();
             String readLine = bufferedReader.readLine();
             while (readLine != null) {
+                Map<String, String> currentAnimalAttributes = new HashMap<>();
                 String[] splittedLine = readLine.split(",");
                 for (int i = 0; i < splittedLine.length; i++) {
-
+                    if(!isExistAttributeValue(splittedLine[i], i)) {
+                        throw new IOException(); //TODO сделать своё исключение
+                    }
+                    currentAnimalAttributes.put(orderedAttributes[i].getTitle(), splittedLine[i]);
                 }
+                entities.add(new AnimalEntity(currentAnimalAttributes));
+                readLine = bufferedReader.readLine();
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return entities;
     }
 
+    public boolean isExistAttributeValue(String attributeValue, int attributeOrderNumber) {
+        return orderedAttributes[attributeOrderNumber].getPossibleValues().contains(attributeValue);
+    }
 
 }
