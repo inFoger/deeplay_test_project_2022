af454f3ce468625e111cf8a5d779a2176e0d7b7b
Angald Anton <anton2801z@gmail.com>
2022-11-16 01:37:52 +0600

    [*] Changes in QueryExecutor class

--- DeeplayTestProject2022/src/main/java/com/github/inFoger/QueryExecutor.java
+++ DeeplayTestProject2022/src/main/java/com/github/inFoger/QueryExecutor.java
@@ -6,28 +6,15 @@ import java.util.List;
 import java.util.logging.Logger;
 
 public class QueryExecutor {
-    private static Logger logger = Logger.getLogger(QueryExecutor.class.getName());
+    private static final Logger logger = Logger.getLogger(QueryExecutor.class.getName());
     public static void execute(List<Query> queryList, List<Entity> entityList) {
         //TODO проверки на нул и пустоту везде, где надо
         for(Query currentQuery : queryList) {
-            if(!isCorrectQuery(currentQuery)){
-                try {
-                    throw new Exception();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
             if(currentQuery.getCommand().equals(TotalEntitiesOperation.getCommandName())) {
                 int result = TotalEntitiesOperation.getTotal(entityList, currentQuery.getFilterParts());
                 logger.info("Result is " + result);
             }
         }
     }
-
-    private static boolean isCorrectQuery(Query query) {
-        //TODO написать проверку
-        return true;
-    }
-
 
 }
